f59f2439d884f6f46d1087519f8697dcd6c3393a
ancrait <xdxddclash@gmail.com>
2026-02-18 22:34:02 +0200

    add user service

---
+++ services/user-service/src/main/java/com/sorokaandriy/user_service/model/UserStatus.java
@@ -0,0 +1,6 @@
+package com.sorokaandriy.user_service.model;
+
+public enum UserStatus {
+    ACTIVE,
+    BLOCKED,
+}
---
+++ services/user-service/src/main/java/com/sorokaandriy/user_service/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sorokaandriy.user_service.repository;
+
+import com.sorokaandriy.user_service.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,String> {
+
+
+}
